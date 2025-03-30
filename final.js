748dd1274e77de8962d35e1275379726e12cf3ea
Mohammad Abdullah-Al-Sajid Chowdhury <75419355+SajidAbdullah729@users.noreply.github.com>
2025-03-30 14:24:53 +0600

    Update final.js

--- final.js
+++ final.js
@@ -2,7 +2,6 @@ const input=document.getElementById("number-input");
 const resulty=document.getElementById("result");
 function handleClickButton(el)
 {
-    /*console.log(el.innerText);*/
     const exp=`${input.value}${el.innerText}`;
     input.value=exp;
     if(['+','-','*','/'].includes(el.innerText)) return;
